16e123912c1830f3303bc380dc3593141a7e3ea8
wade <903879889@qq.com>
2016-08-16 15:56:13 +0800

    test

--- app/src/main/java/com/zhangwei/com/fragmentactivity/MainActivity/Fragment/TestFragment.java
+++ app/src/main/java/com/zhangwei/com/fragmentactivity/MainActivity/Fragment/TestFragment.java
@@ -47,6 +47,7 @@ public class TestFragment extends BaseFragment {
     private void InitView() {
         mTestRecycler = $.findViewById(R.id.testRecycler);
         mTestRecycler = $.findViewById(R.id.testRecycler);
+        mTestRecycler = $.findViewById(R.id.testRecycler);
         mRecyclerView = mTestRecycler.getRecyclerView();
         ltm = new LinearLayoutManager(getActivity());
         mRecyclerView.setLayoutManager(ltm);
